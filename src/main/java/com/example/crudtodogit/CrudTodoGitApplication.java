7d3e679a75e779434de92f262595977eb7648dfd
baker-lee <shishi1340@gmail.com>
2023-01-30 14:48:55 +0900

    Initial commit

---
+++ src/main/java/com/example/crudtodogit/CrudTodoGitApplication.java
@@ -0,0 +1,13 @@
+package com.example.crudtodogit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudTodoGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudTodoGitApplication.class, args);
+    }
+
+}
