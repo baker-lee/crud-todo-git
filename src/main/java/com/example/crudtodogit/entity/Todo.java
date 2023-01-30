51e9517ff77af7678be92b882d9b30d411f800ef
baker-lee <shishi1340@gmail.com>
2023-01-30 15:55:13 +0900

    JPA를 위한 환경설정

--- src/main/java/com/example/crudtodogit/entity/Todo.java
+++ src/main/java/com/example/crudtodogit/entity/Todo.java
@@ -2,8 +2,7 @@ package com.example.crudtodogit.entity;
 
 import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Getter
 @Setter
@@ -11,13 +10,18 @@ import javax.persistence.Id;
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "CRUD_Todo")
 public class Todo {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column
     private String title;
 
+    @Column(name = "todoorder")
     private Long order;
 
+    @Column
     private Boolean completed;
 }
